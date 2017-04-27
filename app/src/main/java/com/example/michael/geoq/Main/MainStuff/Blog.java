3423ac44765d1ef468ab4a2fecb1dfa017fb6f51
Vetox88 <sb.sami.bes@gmail.com>
2017-04-27 11:28:35 +0200

    UP2

--- app/src/main/java/com/example/michael/geoq/Main/MainStuff/Blog.java
+++ app/src/main/java/com/example/michael/geoq/Main/MainStuff/Blog.java
@@ -5,4 +5,46 @@ package com.example.michael.geoq.Main.MainStuff;
  */
 
 public class Blog {
-}
+
+
+        private String desc;
+        private String title;
+
+
+
+        private  String username;
+
+        public Blog(){
+
+        }
+
+        public Blog(String desc, String title) {
+            this.desc = desc;
+            this.title = title;
+            this.username = username;
+        }
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getDesc() {
+            return desc;
+        }
+
+        public void setDesc(String desc) {
+            this.desc = desc;
+        }
+
+    }
