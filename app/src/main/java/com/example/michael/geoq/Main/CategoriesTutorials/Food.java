2ad050b0ddb5dc61f67e4fe312c4c5bfb9af42d2
Vetox88 <sb.sami.bes@gmail.com>
2017-05-03 21:45:50 +0200

    UP7

--- app/src/main/java/com/example/michael/geoq/Main/CategoriesTutorials/Food.java
+++ app/src/main/java/com/example/michael/geoq/Main/CategoriesTutorials/Food.java
@@ -1,14 +1,64 @@
 package com.example.michael.geoq.Main.CategoriesTutorials;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 import com.example.michael.geoq.R;
+
 public class Food extends AppCompatActivity {
+
+// private ListView foodListView;
+
+//    private ArrayList <String> TutorialDesc = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food);
-        
+//
+//        foodListView = (ListView) findViewById(R.id.foodListView);
+//        final String[] Values = new String[]{"Tutorail 1", "Tutorial 2", "Tutorial 3"};
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.activity_list_item, android.R.id.text1, Values);
+//        foodListView.setAdapter(adapter);
+//        foodListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//
+//
+//                startActivity(new Intent(Food.this, FoodTuts.class));
+//
+
+//            }
+//        });
+
+
+
+
+
+//        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, TutorialDesc);
+//        foodListView.setAdapter(arrayAdapter);
+
+
+
+//        ref.orderByValue("Desc").addChildEventListener(new ChildEventListener() {
+//            @Override
+//            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+//
+//
+//                //scoresRef.orderByValue()
+////                String Value = dataSnapshot.getValue(String.class);
+////
+////                TutorialDesc.add(Value);
+////
+////                arrayAdapter.notifyDataSetChanged();
+//
+//
+
+
+
+
+
+
     }
 }
--- app/src/main/java/com/example/michael/geoq/Main/CategoriesTutorials/Repair.java
+++ app/src/main/java/com/example/michael/geoq/Main/CategoriesTutorials/Repair.java
@@ -1,8 +1,8 @@
 package com.example.michael.geoq.Main.CategoriesTutorials;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,24 +13,54 @@ public class Repair extends AppCompatActivity implements View.OnClickListener{
 
 
 private Button buttonTutorial1;
+    public static class Post {
+
+        public String author;
+        public String title;
+
+        public Post(String author, String title) {
+            // ...
+        }
+
+    }
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_repair);
+//        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference ref = database.getReference("https://p2project-36a2e.firebaseio.com/Tutorial_up/REPAIR");
+//        ref.child("REPAIR").addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//
+//
+//                Post post = dataSnapshot.getValue(Post.class);
+//                System.out.println(post);
+//
+//
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//
+//            }
+//        });
 
         buttonTutorial1 = (Button) findViewById(R.id.buttonTutorial1);
 
         buttonTutorial1.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View view) {
 
         if (view == buttonTutorial1) {
-            finish();
             startActivity(new Intent(this, Profile.class));
+
         }
 
     }
